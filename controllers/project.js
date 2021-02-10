40bab245c0d14d8ef485a4045a001fa9ea3d3521
Adrian Mendez <adrianmndz328@gmail.com>
2021-02-09 21:35:42 -0600

    progress: created project controllers

---
+++ controllers/project.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const Project = require('../model/project')
+
+router.get('/', async (req, res) => {
+    res.json(await Project.find({}))
+})
+
+router.get('/:id', async (req, res) => {
+    res.json(await Project.find({ _id: req.params.id }))
+})
+
+router.post('/', async (req, res) => {
+    res.json(await Project.create(req.body))
+})
+
+router.put('/', async (req, res) => {
+    res.json(
+        await Project.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        )
+    )
+})
+
+router.delete('/:id', async (req, res) => {
+    res.json(await Project.findByIdAndDelete(req.params.id))
+})
+
+module.exports = router
