204408da993dac9d5f5474319aa688007c6ece3f
Adrian Mendez <adrianmndz328@gmail.com>
2021-05-20 20:05:16 -0500

    Updated required options

--- model/project.js
+++ model/project.js
@@ -3,8 +3,8 @@ const { Schema, model } = require('mongoose')
 const projectSchema = new Schema({
     name: { type: String, required: true },
     frontend: { type: String, required: true },
-    server: { type: String, required: true },
-    database: { type: String, required: true },
+    server: { type: String, required: false },
+    database: { type: String, required: false },
     image: { type: String, required: true },
     description: { type: String, required: true },
     repo: { type: String, required: true },
